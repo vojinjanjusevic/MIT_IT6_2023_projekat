cca9b0388bca97f36b2869289ef1230f95c02dc4
vojinjanjusevic <janjus1234@gmail.com>
2026-01-20 21:43:08 +0100

    setup

---
+++ lib/screens/root_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class RootScreen extends StatelessWidget {
+  const RootScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text('CarDealer start')),
+    );
+  }
+}
