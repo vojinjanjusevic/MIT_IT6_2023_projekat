e57c119b54c9b87d5add74d96150ea0458903935
vojinjanjusevic <janjus1234@gmail.com>
2026-01-21 14:44:34 +0100

    bottom navbar i 3 screen-a

--- lib/screens/root_screen.dart
+++ lib/screens/root_screen.dart
@@ -1,12 +1,37 @@
 import 'package:flutter/material.dart';
+import 'home_screen.dart';
+import 'my_listings_screen.dart';
+import 'profile_screen.dart';
 
-class RootScreen extends StatelessWidget {
+class RootScreen extends StatefulWidget {
   const RootScreen({super.key});
 
   @override
+  State<RootScreen> createState() => _RootScreenState();
+}
+
+class _RootScreenState extends State<RootScreen> {
+  int index = 0;
+
+  final screens = const [
+    HomeScreen(),
+    MyListingsScreen(),
+    ProfileScreen(),
+  ];
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('CarDealer start')),
+    return Scaffold(
+      body: screens[index],
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: index,
+        onDestinationSelected: (i) => setState(() => index = i),
+        destinations: const [
+          NavigationDestination(icon: Icon(Icons.directions_car), label: 'Oglasi'),
+          NavigationDestination(icon: Icon(Icons.list_alt), label: 'Moji oglasi'),
+          NavigationDestination(icon: Icon(Icons.person_outline), label: 'Profil'),
+        ],
+      ),
     );
   }
 }
